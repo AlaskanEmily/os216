7e5bfbd35135c95503a4cac9a4111f55d1ebc3b1
AlaskanEmily <emily@alaskanemily.net>
2019-02-28 00:37:53 -0800

    [x86] Add single-entry freelist cache to physical memory manager

--- nanokernel/x86/os216_nano_physmem.s
+++ nanokernel/x86/os216_nano_physmem.s
@@ -28,15 +28,16 @@ global OS216_Nano_AllocatePhysPage
 global OS216_Nano_FreePhysPage
 global OS216_Nano_MarkPhysPage
 
+PAGE_SHIFT equ 12
 PAGE_SIZE equ 4096
 PAGE_BITMASK_SIZE equ 97984
 PAGE_BITMASK_32_SIZE equ (PAGE_BITMASK_SIZE/4)
 
-PAGE_LOW_BIT_SIZE equ 192
-PAGE_LOW_BIT_32_SIZE equ (PAGE_LOW_BIT_SIZE/4)
-PAGE_LOW_BYTE_SIZE equ (PAGE_LOW_BIT_SIZE*8)
+PAGE_LO_BYTE_SIZE equ 192
+PAGE_LO_BYTE_32_SIZE equ (PAGE_LO_BYTE_SIZE/4)
+PAGE_LO_BIT_SIZE equ (PAGE_LO_BYTE_SIZE*8)
 
-PAGE_LOW_START equ 0x00900000
+PAGE_LO_START equ 0x00900000
 PAGE_HI_START equ 0x01000000
 
 segment .text
@@ -50,68 +51,87 @@ segment .text
 ; That totals 97984 bytes to hold all the bits, 192 bytes of which are below ISA.
 
 OS216_Nano_AllocatePhysPage:
+    ; Find out of we can just reuse the freed index
+    push edi
+    mov edi, last_freed_index
+    stosd
+    mov ecx, eax
+    jecxz .calculate_next_index
+    ; last_freed_index is usable
+    xor ecx, ecx
+    mov [edi-4], ecx
+    dec eax
+    jmp compute_address_from_bitmask
+
+.calculate_next_index
+    ; Find the first open index. Set stuff up for wacky x86iness
+    mov edi, memory_map
+    mov ecx, PAGE_BITMASK_32_SIZE
+    ; Get all F's into eax
     xor eax, eax
-    jmp .find_page_begin
-.find_page_iter:
+    dec eax
+    repne stosd ; Find the first non-filled byte
+    not eax
+    push ecx ; We need ecx for the shl below
+    bsf ecx, eax ; Get the bit index into ecx
+    ; Get 1 into eax
+    xor eax, eax
     inc eax
-    ; Check if we are outside the range of the bitmask.
-    cmp eax, PAGE_BITMASK_32_SIZE
-    jge .oom
-.find_page_begin:
-    mov ecx, [memory_map+eax*4]
-    not ecx
-    jecxz .find_page_iter
-    bsf edx, ecx
-    
-    ; Set that this page is used.
-    not ecx
-    bts ecx, edx
-    mov [memory_map+eax*4], ecx
-    
-    mov ecx, PAGE_LOW_START
-    ; Check if we are in the low map
-    cmp eax, PAGE_LOW_BIT_32_SIZE
-    jl .low_page
-    mov ecx, PAGE_HI_START
-.low_page:
-    ; eax is an index for ever 32 bits.
-    ; shift by 5 to change it to a bit offset
-    shl eax, 5
-    ; add edx for the index within the 32 bits eax points at
-    add eax, edx
-    ; shift by 12 for the page size.
-    shl eax, 12
-    ; Add the base address.
-    add eax, ecx
+    shl eax, cl ; Coincidentally, ecx is the only register we can use for a shift.
+    ; Fill in the entry
+    or [edi-4], eax
+    mov eax, ecx ; Put the byte index into eax...
+    pop ecx
+    ; eax (al) is the 5 low bits of the index
+    ; PAGE_BITMASK_32_SIZE-ecx is the upper bits
+    neg ecx
+    add ecx, PAGE_BITMASK_32_SIZE
+    shl ecx, 5
+    or eax, ecx
+compute_address_from_bitmask:
+    shl eax, PAGE_SHIFT
+    cmp eax, (PAGE_LO_BIT_SIZE<<PAGE_SHIFT)
+    jg .hi_address
+.lo_address
+    add eax, PAGE_LO_START
+    pop edi
     ret
-.oom:
-    xor eax, eax
+    
+.hi_address:
+    add eax, PAGE_HI_START
+    pop edi
     ret
+
+.last_freed_index_valid:
 
 ; Expects the address in eax, returns the resulting byte in eax and bit in ecx.
 ; Doesn't touch any other registers.
 phys_to_bit:
-    mov ecx, PAGE_LOW_START
-    cmp eax, PAGE_HI_START
-    jl .low_page
-    mov ecx, PAGE_HI_START-(PAGE_SIZE*PAGE_LOW_BYTE_SIZE)
-.low_page:
+    shr eax, PAGE_SHIFT
+    mov ecx, (PAGE_HI_START>>PAGE_SHIFT)
+    cmp eax, ecx
+    jl .lo_address
     sub eax, ecx
-    shr eax, 15
-    shr ecx, 12
-    and ecx, 0xFF
+    add eax, PAGE_LO_BIT_SIZE
+    jmp .bitmask_index_set
+.lo_address:
+    sub eax, (PAGE_LO_START>>PAGE_SHIFT)
+.bitmask_index_set:
+    mov ecx, eax
+    shr eax, 5
+    and ecx, 0x1F ; 31
     ret
 
 OS216_Nano_FreePhysPage:
     mov eax, [esp+4]
     call phys_to_bit
-    btc [memory_map+eax], ecx
+    btc [memory_map+eax*4], ecx
     ret
 
 OS216_Nano_MarkPhysPage:
     mov eax, [esp+4]
     call phys_to_bit
-    bts [memory_map+eax], ecx
+    bts [memory_map+eax*4], ecx
     ret
 
 segment .rodata
@@ -119,4 +139,5 @@ segment .rodata
 
 segment .bss
     os216_phys_memory_size: resd 1
+    last_freed_index: resd 1
     memory_map: resb PAGE_BITMASK_SIZE
